c0c31eb11e51558e5dbb56b135009d58f2b28167
Aldea Alexia <alexia.aldea@mready.net>
2024-10-30 17:03:39 +0200

    first commit

---
+++ Mobile/Smarthack2025/Smarthack2025/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  Smarthack2025App.swift
+//  Smarthack2025
+//
+//  Created by Aldea Alexia on 30.10.2024.
+//
+
+import SwiftUI
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//        NFX.sharedInstance().start()
+        
+        return true
+    }
+    
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
