c7a217d5725b12aecc660277e86e7c60f1dfabd6
Ana Varban <ana.varban@mready.net>
2024-11-02 15:08:16 +0200

    added parser

---
+++ ceva/classes.py
@@ -0,0 +1,72 @@
+class Connection:
+    def __init__(self, connection_id, from_id, to_id, distance, lead_time_days, connection_type, max_capacity):
+        self.connection_id = connection_id
+        self.from_id = from_id
+        self.to_id = to_id
+        self.distance = distance
+        self.lead_time_days = lead_time_days
+        self.connection_type = connection_type
+        self.max_capacity = max_capacity
+
+    def __str__(self):
+        return f"Connection {self.connection_id} from {self.from_id} to {self.to_id} with lead time {self.lead_time_days} days, type {self.connection_type} and max capacity {self.max_capacity}"
+
+class Customer:
+    def __init__(self, customer_id, name, max_input,over_input_penalty,late_delivery_penalty,early_delivery_penalty,node_type):
+        self.customer_id = customer_id
+        self.name = name
+        self.max_input = max_input
+        self.over_input_penalty = over_input_penalty
+        self.late_delivery_penalty = late_delivery_penalty
+        self.early_delivery_penalty = early_delivery_penalty
+        self.node_type = node_type
+
+    def __str__(self):
+        return f"Customer {self.customer_id} {self.name} with max input {self.max_input}, over input penalty {self.over_input_penalty}, late delivery penalty {self.late_delivery_penalty}, early delivery penalty {self.early_delivery_penalty} and node type {self.node_type}"
+
+class Refinery:
+    def __init__(self, refinery_id, name,capacity,max_output,production,overflow_penalty,underflow_penalty,over_output_penalty,production_cost,production_co2,initial_stock,node_type):
+        self.refinery_id = refinery_id
+        self.name = name
+        self.capacity = capacity
+        self.max_output = max_output
+        self.production = production
+        self.overflow_penalty = overflow_penalty
+        self.underflow_penalty = underflow_penalty
+        self.over_output_penalty = over_output_penalty
+        self.production_cost = production_cost
+        self.production_co2 = production_co2
+        self.initial_stock = initial_stock
+        self.node_type = node_type
+
+    def __str__(self):
+        return f"Refinery {self.refinery_id} {self.name} with capacity {self.capacity}, max output {self.max_output}, production {self.production}, overflow penalty {self.overflow_penalty}, underflow penalty {self.underflow_penalty}, over output penalty {self.over_output_penalty}, production cost {self.production_cost}, production co2 {self.production_co2}, initial stock {self.initial_stock} and node type {self.node_type}"
+
+class Tanks:
+    def __init__(self, tank_id, name,capacity,max_input,max_output,overflow_penalty,underflow_penalty,over_input_penalty,over_output_penalty,initial_stock,node_type):
+        self.tank_id = tank_id
+        self.name = name
+        self.capacity = capacity
+        self.max_input = max_input
+        self.max_output = max_output
+        self.overflow_penalty = overflow_penalty
+        self.underflow_penalty = underflow_penalty
+        self.over_input_penalty = over_input_penalty
+        self.over_output_penalty = over_output_penalty
+        self.initial_stock = initial_stock
+        self.node_type = node_type
+
+    def __str__(self):
+        return f"Tanks {self.tank_id} {self.name} with capacity {self.capacity}, max input {self.max_input}, max output {self.max_output}, overflow penalty {self.overflow_penalty}, underflow penalty {self.underflow_penalty}, over input penalty {self.over_input_penalty}, over output penalty {self.over_output_penalty}, initial stock {self.initial_stock} and node type {self.node_type}"
+
+class Demand:
+    def __init__(self, demand_id, customer_id,quantity,post_day,start_delivery_day,end_delivery_day):
+        self.demand_id = demand_id
+        self.customer_id = customer_id
+        self.quantity = quantity
+        self.post_day = post_day
+        self.start_delivery_day = start_delivery_day
+        self.end_delivery_day = end_delivery_day
+
+    def __str__(self):
+        return f"Demand {self.demand_id} for customer {self.customer_id} with quantity {self.quantity}, post day {self.post_day}, start delivery day {self.start_delivery_day} and end delivery day {self.end_delivery_day}"
---
+++ ceva/parser.py
@@ -0,0 +1,152 @@
+import csv
+from classes import Connection, Customer, Refinery, Tanks, Demand
+
+def parse_connections(file_path):
+    with open(file_path, 'r') as file:
+        csv_reader = csv.reader(file,delimiter=';', quotechar='"')
+        
+        next(csv_reader, None)
+        
+        for row in csv_reader:
+            try:
+                yield Connection(
+                    row[0],
+                    row[1],
+                    row[2],
+                    row[3],
+                    row[4],
+                    row[5],
+                    row[6]
+                )
+            except IndexError as e:
+                print(f"Error processing row {row}: {e}")
+                continue
+
+connections_path = '/Users/mready/Downloads/challenge-main/eval-platform/src/main/resources/liquibase/data/connections.csv'
+connections_data = list(parse_connections(connections_path))
+
+def parse_customers(file_path):
+    with open(file_path, 'r') as file:
+        csv_reader = csv.reader(file,delimiter=';', quotechar='"')
+        
+        next(csv_reader, None)
+        
+        for row in csv_reader:
+            try:
+                yield Customer(
+                    row[0],
+                    row[1],
+                    row[2],
+                    row[3],
+                    row[4],
+                    row[5],
+                    row[6]
+                )
+            except IndexError as e:
+                print(f"Error processing row {row}: {e}")
+                continue
+
+customers_path = '/Users/mready/Downloads/challenge-main/eval-platform/src/main/resources/liquibase/data/customers.csv'
+customers_data = list(parse_customers(customers_path))
+
+def parse_refineries(file_path):
+    with open(file_path, 'r') as file:
+        csv_reader = csv.reader(file,delimiter=';', quotechar='"')
+        
+        next(csv_reader, None)
+        
+        for row in csv_reader:
+            try:
+                yield Refinery(
+                    row[0],
+                    row[1],
+                    row[2],
+                    row[3],
+                    row[4],
+                    row[5],
+                    row[6],
+                    row[7],
+                    row[8],
+                    row[9],
+                    row[10],
+                    row[11]
+                )
+            except IndexError as e:
+                print(f"Error processing row {row}: {e}")
+                continue
+
+refineries_path = '/Users/mready/Downloads/challenge-main/eval-platform/src/main/resources/liquibase/data/refineries.csv'
+refineries_data = list(parse_refineries(refineries_path))
+
+def parse_tanks(file_path):
+    with open(file_path, 'r') as file:
+        csv_reader = csv.reader(file,delimiter=';', quotechar='"')
+        
+        next(csv_reader, None)
+        
+        for row in csv_reader:
+            try:
+                yield Tanks(
+                    row[0],
+                    row[1],
+                    row[2],
+                    row[3],
+                    row[4],
+                    row[5],
+                    row[6],
+                    row[7],
+                    row[8],
+                    row[9],
+                    row[10]
+                )
+            except IndexError as e:
+                print(f"Error processing row {row}: {e}")
+                continue 
+
+tanks_path = '/Users/mready/Downloads/challenge-main/eval-platform/src/main/resources/liquibase/data/tanks.csv'
+tanks_data = list(parse_tanks(tanks_path))
+
+def parse_first_demands(file_path):
+    with open(file_path, 'r') as file:
+        csv_reader = csv.reader(file,delimiter=';', quotechar='"')
+        
+        next(csv_reader, None)
+        for row in csv_reader:
+            try:
+                if float(row[3]) == 0:
+                    yield Demand(
+                        row[0],
+                        row[1],
+                        row[2],
+                        row[3],
+                        row[4],
+                        row[5],
+                    )
+            except IndexError as e:
+                print(f"Error processing row {row}: {e}")
+                continue
+
+first_demands_path = '/Users/mready/Downloads/challenge-main/eval-platform/src/main/resources/liquibase/data/demands.csv'
+first_demands_data = list(parse_first_demands(first_demands_path))
+
+def print_output(): # to test parsing, run it forwarding the output to a file
+    print(f"Parsed connections: {len(connections_data)}")
+    for i, connection in enumerate(connections_data):
+        print(f"Connection {i}: {connection}")
+
+    print(f"Parsed customers: {len(customers_data)}")
+    for i, customer in enumerate(customers_data):
+        print(f"Customer {i}: {customer}")
+
+    print(f"Parsed refineries: {len(refineries_data)}")
+    for i, refinery in enumerate(refineries_data):
+        print(f"Refinery {i}: {refinery}")
+
+    print(f"Parsed tanks: {len(tanks_data)}")
+    for i, tank in enumerate(tanks_data):
+        print(f"Tank {i}: {tank}")
+
+    print(f"Parsed first demands: {len(first_demands_data)}")
+    for i, demand in enumerate(first_demands_data):
+        print(f"Demand {i}: {demand}")
+        
